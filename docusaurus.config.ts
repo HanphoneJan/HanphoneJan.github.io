8ae1eb508dca508ec9b84fe95a232ba1ca2b7c9f
寒枫 <1195560097@qq.com>
2026-02-13 12:34:22 +0800

    目录更新

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,12 +2,14 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...
 
 const config: Config = {
   title: 'HanphoneJan 技术文档',
   tagline: '技术文档与知识分享',
   favicon: 'img/favicon.ico',
+
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -20,14 +22,17 @@ const config: Config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
+
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'hanphonejan', // Usually your GitHub org/user name.
   projectName: 'HanphoneJan.github.io', // Usually your repo name.
   deploymentBranch: 'gh-pages',
 
+
   onBrokenLinks: 'throw',
   onDuplicateRoutes: 'warn',
+
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -37,6 +42,7 @@ const config: Config = {
     locales: ['zh-CN'],
   },
 
+
   markdown: {
     mermaid: true,
     format: 'detect',
@@ -45,7 +51,9 @@ const config: Config = {
     },
   },
 
+
   themes: ['@docusaurus/theme-mermaid'],
+
 
   presets: [
     [
@@ -72,6 +80,7 @@ const config: Config = {
       } satisfies Preset.Options,
     ],
   ],
+
 
   themeConfig: {
     colorMode: {
@@ -120,5 +129,6 @@ const config: Config = {
     },
   } satisfies Preset.ThemeConfig,
 };
+
 
 export default config;
